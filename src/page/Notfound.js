d8dc3931163341bff0b7628eedfcfb3b2e224c44
Difarft <Difarft2>
2024-09-06 06:47:56 +0700

    pembuatan tab notfound full

---
+++ src/page/Notfound.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Copyright from "../components/copyright";
+import NF from "../components/nf";
+
+
+const NotFound = () => {
+    return (
+        <div className="">
+            <NF/>
+            <Copyright/>
+        </div>
+    );
+};
+
+export default NotFound;
