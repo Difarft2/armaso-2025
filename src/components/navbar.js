4335fd3cdbf13ef70ba1330df89c249a2c54bc7e
Revanjoharil <revanj.aqdam@gmail.com>
2024-09-05 23:27:23 +0700

    pembaruan dan styling navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,46 @@
 import "../css/navbar.css"
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
-const Navbar =()=>{
+
+
+const Navbarr =()=>{
     return(
         <nav>
-            <div className="Logo">
+            {
+            
+            <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+              <Navbar.Brand href="/">
+          <img
+            src="/path/to/your/logo.png"
+            alt="Armaso"
+            width="40"
+            height="40"
+            className="logo armaso"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="#timeline">Time Line</Nav.Link>
+                  <Nav.Link href="#aboutus">About Us</Nav.Link>
+                  <Nav.Link href="#menumapel">Subject</Nav.Link>
+                </Nav>
+                <nav className="button">
+                    <Button variant="secondary">Login</Button>{' '}
+                    <Button variant="secondary">Register</Button>{' '}
+                </nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+
+            /* <div className="Logo">
 
             </div>
 
@@ -20,9 +55,9 @@ const Navbar =()=>{
             <div className="Pendaftaran">
                 <button className="Login">LOGIN</button>
                 <button className="Register">REGISTER</button>
-            </div>
+            </div> */}
         </nav>
     )
 }
 
-export default Navbar;
+export default Navbarr;
