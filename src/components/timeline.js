cf79b5acba3c804e0f354e5ce9270efb4dfe9313
Difarft <dimasfata12345@gmail.com>
2024-09-07 16:11:34 +0700

    Pembuatan Countdown

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,11 +1,15 @@
 import "../css/timeline.css"
+import Countdownt from "./countdown";
 import TL from "./TL";
 
 
 const Timeline =()=>{
+    const targetDate = "2024-12-31T00:00:00";
+
     return(
         <section className="timeline-container" id="timeline">
             <TL/>
+            <Countdownt targetDate={targetDate}/>
         </section>
     )
 }
