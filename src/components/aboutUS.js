89736409e2f835243dbe7774241e416bc5b1ec5d
Revanjoharil <revanj.aqdam@gmail.com>
2024-09-15 12:03:27 +0700

    nabar ahirrrrrr

--- src/components/aboutUS.js
+++ src/components/aboutUS.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "../css/aboutus.css"
 import Button from 'react-bootstrap/Button';
-
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../css/aboutus.css";
 
 const ExampleCarouselImage = ({ text }) => {
   return (
@@ -54,6 +54,15 @@ const UncontrolledExample = () => {
 };
 
 const ResponsivePage = () => {
+  const [showModal1, setShowModal1] = useState(false);
+  const [showModal2, setShowModal2] = useState(false);
+
+  const handleCloseModal1 = () => setShowModal1(false);
+  const handleShowModal1 = () => setShowModal1(true);
+
+  const handleCloseModal2 = () => setShowModal2(false);
+  const handleShowModal2 = () => setShowModal2(true);
+
   return (
     <div className="container-fluid" id='aboutus'>
       <h1>Do you know Armaso?</h1>
@@ -66,11 +75,47 @@ const ResponsivePage = () => {
             ARMASO (Ar-Rahmat Mathematic, Science, and Social Olympiad) ini merupakan olimpiade tingkat SD Sederajat yang diadakan oleh Pondok Pesantren Modern Ar Rahmat Bojonegoro. Melalui ARMASO 2023 diharapkan para siswa sebagai generasi muda penerus bangsa dapat memiliki kompetensi dan kecerdasan dalam memahami isu-isu yang berkaitan dengan Matematika Sains dan Sosial.
           </p>
           <div className="buttons">
-          <Button variant="secondary">Login</Button>{' '}
-          <Button variant="secondary">Register</Button>{' '}
+            <Button variant="secondary" onClick={handleShowModal1}>
+              Syarat & Ketentuan
+            </Button>{' '}
+            <Button variant="secondary" onClick={handleShowModal2}>
+              Buku Panduan
+            </Button>
           </div>
         </div>
       </div>
+
+      {/* Modal 1 */}
+      <Modal show={showModal1} onHide={handleCloseModal1} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Syarat & Ketentuan</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Syarat & ketentuan akan ditampilkan dalam bentuk PDF. Klik tombol bertuliskan "Open PDF" di bawah untuk membukanya!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal1}>
+            Close
+          </Button>
+          <Button href='https://armaso.arrahmat-bjn.sch.id/mainPage/PDF/SYARAT.pdf' variant="secondary" onClick={handleCloseModal1}>
+            Open PDF
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Modal 2 */}
+      <Modal show={showModal2} onHide={handleCloseModal2} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Buku Panduan</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Buku panduan akan ditampilkan dalam bentuk PDF. Klik tombol bertuliskan "Open PDF" di bawah untuk membukanya!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal2}>
+            Close
+          </Button>
+          <Button href='https://armaso.arrahmat-bjn.sch.id/mainPage/PDF/SYARAT.pdf' variant="secondary" onClick={handleCloseModal1}>
+            Open PDF
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
--- src/components/contactus.js
+++ src/components/contactus.js
@@ -1,80 +1,45 @@
-import "../css/contactus.css"
-import { Link } from "react-router-dom";
+import "../css/contactus.css";
 
-import igIco from "../img/ico/instagram-logo.png"
-import ytIco from "../img/ico/youtube.png"
-import emailico from "../img/ico/email.png"
-import glbico from "../img/ico/globe.png"
-import mapico from "../img/ico/map.png"
-import waico from "../img/ico/whatsapp.png"
+const ContactCard = ({ title, contacts }) => {
+  return (
+    <div className="contact-card">
+    
+    <div className="contact-list">
+      <div className="contact-item">
+        <div className="contact-name">
+          <img src="line-icon.png" alt="Line" />
+          <span>halizaapalwan (Liza)</span>
+        </div>
+        <div className="contact-info">
+          <a href="https://wa.me/081922" className="contact-button">
+            <img src="whatsapp-icon.png" alt="WhatsApp" />
+            081922 (Terate)
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  );
+};
 
-const ContactUS = () => {
-    return ( <
-        section className = "contactUS-container"
-        id = "contactus" >
-            <div className="bagKanan-container">
-                <p>Contact US</p>
+function App() {
+  const onlineContacts = [
+    { name: 'Liza', icon: 'line-icon.png', phone: '085198359684' },
+  ];
 
-                <div className="noWa-1">
-                    <img className="wa-1" src={waico} alt="wa" />
-                    <Link to="#" target="_blank">Admin 1</Link>
-                </div>
+  const offlineContacts = [
+    { name: 'Anindya', icon: 'line-icon.png', phone: '082337081505' },
+    { name: 'Kore', icon: 'line-icon.png', phone: '082312708285' },
+  ];
 
-                <div className="noWa-2">
-                    <img className="wa-2" src={waico} alt="wa" />
-                    <Link to="#" target="_blank">Admin 2</Link>
-                </div>
-
-                <div className="noWa-3">
-                    <img className="wa-3" src={waico} alt="wa" />
-                    <Link to="#" target="_blank">Admin 3</Link>
-                </div>
-            </div>
-
-            <div className="bagTengah-container">
-                <p>Other Link</p>
-
-                <div className="maps">
-                    <img className="map" src={mapico} alt="map" />
-                    <Link to="https://www.google.com/maps/place/SMP-SMA+Plus+Ar+Rahmat/@-7.1576623,111.8758344,17z/data=!3m1!4b1!4m6!3m5!1s0x2e7781f2f1a405b1:0x819ab1678038f906!8m2!3d-7.1576676!4d111.8784093!16s%2Fg%2F1hm4rd1kh?hl=id&entry=tts" target="_blank">Maps</Link>
-                </div>
-
-                <div className="website">
-                    <img className="web" src={glbico} alt="website Arrahmat" />
-                    <Link to="https://www.arrahmat-bjn.sch.id/" target="_blank">Arrahmat</Link>
-                </div>
-
-                <div className="mail">
-                    <img className="mail-ico" src={emailico} alt="mail" />
-                    <Link to="mailto:armasoarrahmatbjn@gmail.com">Mail</Link>
-                </div>
-            </div>
-
-            <div className="bagKiri-container">
-                <p>Media Sosial</p>
-
-                <div className="ig-armaso">
-                    <img className="armaso-ig" src={igIco} alt="ig-armaso" />
-                    <Link to="https://www.instagram.com/armaso_arrahmat/" target="_blank">Armaso</Link>
-                </div>
-
-                <div className="ig-arrahmat">
-                    <img className="arrahmat-ig" src={igIco} alt="ig-arrahmat" />
-                    <Link to="https://www.instagram.com/arrahmatofficial/" target="_blank">ArrahmatOfficial</Link>
-                </div>
-
-                <div className="ig-oppra">
-                    <img className="oppra-ig" src={igIco} alt="ig-oppra" />
-                    <Link to="https://www.instagram.com/oppra_arrahmat/" target="_blank">Oppra</Link>
-                </div>
-
-                <div className="yt-arrahmat">
-                    <img className="arrahmat-yt" src={ytIco} alt="yt=arrahmat" />
-                    <Link to="https://www.youtube.com/@ArRahmatOfficial" target="_blank">ArrahmatOfficial</Link>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <div className="App">
+      <h1>Contact Person</h1>
+      <ContactCard title="Contact Person Penyisihan NLC Online" contacts={onlineContacts} />
+      <ContactCard title="Contact Person Penyisihan NLC Offline" contacts={offlineContacts} />
+    </div>
+  );
 }
 
-export default ContactUS;
+export default App;
