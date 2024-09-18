f931188ffeb0d23fdfa05f6fa550835ac299e5d5
Difarft <dimasfata12345@gmail.com>
2024-09-18 11:13:56 +0700

    cd timline ahirrr

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -4,12 +4,11 @@ import TL from "./TL";
 
 
 const Timeline =()=>{
-    const targetDate = "2024-12-31T00:00:00";
 
     return(
         <section className="timeline-container" id="timeline">
             <TL/>
-            <Countdownt targetDate={targetDate}/>
+            <Countdownt/>
         </section>
     )
 }
