f1f45db4ce6b5d33bfbad4a4aa21f074a2e9d8b1
Difarft <dimasfata12345@gmail.com>
2024-09-20 13:57:17 +0700

    parallax aboutus

---
+++ src/setting/otherlink.js
@@ -0,0 +1,4 @@
+// Link PDF
+
+export const bukupanduan = "https://armaso.arrahmat-bjn.sch.id/mainPage/PDF/SYARAT.pdf"
+export const syaratdanketentuan = "https://armaso.arrahmat-bjn.sch.id/mainPage/PDF/SYARAT.pdf"
