bb1f167a36ead0532e75496c4b32a41bb386e325
Difarft <dimasfata12345@gmail.com>
2024-09-30 09:00:14 +0700

    loading...

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,5 +1,3 @@
-import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import "../css/home.css";
 
 import Navbar from "../components/navbar";
@@ -7,25 +5,17 @@ import ContactUS from "../components/contactus";
 import Copyright from "../components/copyright";
 import Parallax3 from "../animation/Parallax3";
 
+import Loading1 from "../animation/loading1";
+
 const Home = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    const hash = location.hash;
-    if (hash) {
-      const element = document.querySelector(hash);
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
-      }
-    }
-  }, [location]);
-
   return (
     <div>
-      <Navbar/>
-      <Parallax3/>
-      <ContactUS />
-      <Copyright />
+      <Loading1>
+        <Navbar/>
+        <Parallax3/>
+        <ContactUS />
+        <Copyright />
+      </Loading1>
     </div>
   );
 };
