4391473aafd7a8619fd848af0944ed55f2384038
Difarft <dimasfata12345@gmail.com>
2024-10-03 21:26:16 +0700

    contactus

--- src/setting/otherlink.js
+++ src/setting/otherlink.js
@@ -1,4 +1,20 @@
 // Link PDF
-
 export const bukupanduan = "https://armaso.arrahmat-bjn.sch.id/mainPage/PDF/SYARAT.pdf"
 export const syaratdanketentuan = "https://armaso.arrahmat-bjn.sch.id/mainPage/PDF/SYARAT.pdf"
+
+//link wa
+export const wa1 = ""
+export const wa2 = ""
+
+//other
+export const webarahmat = "https://arrahmat-bjn.sch.id/"
+export const lokasi = "https://www.google.com/maps/place/SMP-SMA+Plus+Ar+Rahmat"
+
+//mail
+export const mail = "mailto:armasoarrahmatbjn@gmail.com"
+
+//mediasosial
+export const igoppra = "https://www.instagram.com/oppra_arrahmat/"
+export const igarmaso = "https://www.instagram.com/armaso_arrahmat/"
+export const igarrahmat = "https://www.instagram.com/arrahmat_bjn/"
+export const ytarrahmat = "https://www.youtube.com/@ArRahmatOfficial"
